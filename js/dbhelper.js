4dbfa0d90d89d7888f8de36021e4b2f77bc837d6
Vikram Deshmukh <srvikram13@gmail.com>
2018-06-26 10:12:17 -0700

    fix: Updated port number as requested

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 90 // Change this to your server port
+    const port = 9000 // Change this to your server port
     return `//localhost:${port}/data/restaurants.json`;
   }
 
